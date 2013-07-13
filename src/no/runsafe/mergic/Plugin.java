cd0bca9e65efb53c31b2e7194ee21e88deaa3166
Kruithne <kruithne@gmail.com>
2013-07-13 18:36:36 +0100

    Initialized new plugin Mergic from template

---
+++ src/no/runsafe/mergic/Plugin.java
@@ -0,0 +1,12 @@
+package no.runsafe.mergic;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class Plugin extends RunsafePlugin
+{
+	@Override
+	protected void PluginSetup()
+	{
+		//addComponent(SomeComponent.class);
+	}
+}

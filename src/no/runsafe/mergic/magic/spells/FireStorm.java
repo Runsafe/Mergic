a3aa2311ba38d45a08e34da6285c28ca780e849b
Kruithne <kruithne@gmail.com>
2013-12-29 03:42:48 +0000

    Try Lava.

--- src/no/runsafe/mergic/magic/spells/FireStorm.java
+++ src/no/runsafe/mergic/magic/spells/FireStorm.java
@@ -8,7 +8,7 @@ public class FireStorm extends Storm
 {
 	public FireStorm(IServer server)
 	{
-		super(server, Item.Unavailable.Fire);
+		super(server, Item.Unavailable.StationaryLava);
 	}
 
 	@Override

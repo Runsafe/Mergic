70f345fce1d92ac6295e42aa873743c0e2e0b2df
ExposedGun316 <exposedgun316@live.co.uk>
2014-09-10 20:18:32 +0100

    Create EarthStorm.java

---
+++ src/no/runsafe/mergic/magic/spells/EarthStorm.java
@@ -0,0 +1,26 @@
+package no.runsafe.mergic.magic.spells;
+
+import no.runsafe.framework.api.IServer;
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.mergic.KillManager;
+import no.runsafe.mergic.magic.MagicSchool;
+
+public class EarthStorm extends Storm
+{
+	public NatureStorm(IServer server, KillManager manager)
+	{
+		super(server, Item.BuildingBlock.Grass, manager);
+	}
+
+	@Override
+	public String getName()
+	{
+		return "Earth Storm";
+	}
+
+	@Override
+	public MagicSchool getSchool()
+	{
+		return MagicSchool.NATURE;
+	}
+}
